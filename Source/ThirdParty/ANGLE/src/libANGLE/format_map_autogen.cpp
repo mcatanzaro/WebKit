5a29a3d32e2be6035b2017846300e1703254de2f
Michael Catanzaro <mcatanzaro@gnome.org>
2026-09-09 15:44:09 -0500

    Revert "Cherry-pick 305413.982@safari-7624.5.1.10-branch (8b1c27595893). https://bugs.webkit.org/show_bug.cgi?id=315712"
    Unreviewed stable branch commit.
    
    This reverts commit 37a200263b1c424bb5ef91ffc0d1bb71f78b09eb.
    
    This is a temporary revert to reduce conflicts. I will reland
    imminently.

--- Source/ThirdParty/ANGLE/src/libANGLE/format_map_autogen.cpp
+++ Source/ThirdParty/ANGLE/src/libANGLE/format_map_autogen.cpp
@@ -762,8 +762,18 @@ bool ValidES3FormatCombination(GLenum format, GLenum type, GLenum internalFormat
                     {
                         case GL_DEPTH_COMPONENT24:
                         case GL_DEPTH_COMPONENT16:
-                        case GL_DEPTH_COMPONENT32_OES:
                         case GL_DEPTH_COMPONENT:
+                            return true;
+                        default:
+                            break;
+                    }
+                    break;
+                }
+                case GL_UNSIGNED_INT_24_8:
+                {
+                    switch (internalFormat)
+                    {
+                        case GL_DEPTH_COMPONENT32_OES:
                             return true;
                         default:
                             break;
