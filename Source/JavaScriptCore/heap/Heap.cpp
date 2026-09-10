30084183ed393374d7184bb2d55fd2c3be2100c1
Justin Michaud <jmichaud@igalia.com>
2026-09-10 03:49:22 -0700

    Cherry-pick 320724@main (249ede976fcc). https://bugs.webkit.org/show_bug.cgi?id=321012
        Replace sched_yield with nanosleep and abstract away spin lock logic on all platforms
        https://bugs.webkit.org/show_bug.cgi?id=321012
    
        Reviewed by Yusuke Suzuki.
    
        sched_yield is not so helpful, since it isn't guaranteed
        to actually yield (unlike the macOS thread_switch) and can
        cause deadlocks.
    
        Darwin's tier setup remains mostly unchanged:
    
        Tier 1: spinloop
        Tier 2: thread_switch
        Tier 3: Parking lot if available (i.e, nothing for bmalloc)
    
        or
    
        Tier 1: spinloop
        Tier 2A: pause (avoid hammering the cache)
        Tier 2B: thread_switch
    
        (depending on the location)
    
        Linux now has:
        Tier 1: spinloop
        Tier 2: waiting with backoff to avoid hammering the cache
        Tier 3: parking lot OR thread_yield
    
        Thread::yield now definitely always yields, but it is slower.
    
        We acomplish this by making SpinBackoff, which abstracts this logic
        away.
    
        We then extend it to a few other places for consistency.
    
        This is measured to be perf-neutral on MBA M4, Neoverse N1 (linux) and RPI4/5 64-bit.
    
        * Source/JavaScriptCore/heap/Heap.cpp:
        (JSC::Heap::clearConcurrentRetainedDataIfPossible):
        * Source/JavaScriptCore/runtime/JSLock.cpp:
        (JSC::JSLock::grabAllLocks):
        * Source/WTF/WTF.xcodeproj/project.pbxproj:
        * Source/WTF/wtf/CMakeLists.txt:
        * Source/WTF/wtf/LockAlgorithmInlines.h:
        (WTF::Hooks>::lockSlow):
        * Source/WTF/wtf/SpinBackoff.h: Added.
        (WTF::SpinBackoff::shouldParkAfterSpinOnce):
        (WTF::SpinBackoff::spinOnce):
        * Source/WTF/wtf/WordLock.cpp:
        (WTF::WordLock::lockSlow):
        (WTF::WordLock::unlockSlow):
        * Source/WTF/wtf/posix/ThreadingPOSIX.cpp:
        (WTF::Thread::yield):
        * Source/bmalloc/CMakeLists.txt:
        * Source/bmalloc/bmalloc.xcodeproj/project.pbxproj:
        * Source/bmalloc/bmalloc/Mutex.cpp:
        (bmalloc::Mutex::lockSlowCase):
        (bmalloc::yield): Deleted.
        * Source/bmalloc/bmalloc/SpinBackoff.h: Added.
        (bmalloc::SpinBackoff::shouldParkAfterSpinOnce):
        (bmalloc::SpinBackoff::spinOnce):
        (bmalloc::SpinBackoff::cpuPause):
        (bmalloc::SpinBackoff::platformYield):
        (bmalloc::SpinBackoff::yield):
    
        Canonical link: https://commits.webkit.org/320724@main
    
    Canonical link: https://commits.webkit.org/317695.254@webkitglib/2.54

--- Source/JavaScriptCore/heap/Heap.cpp
+++ Source/JavaScriptCore/heap/Heap.cpp
@@ -1,6 +1,7 @@
 /*
  *  Copyright (C) 2003-2026 Apple Inc. All rights reserved.
  *  Copyright (C) 2007 Eric Seidel <eric@webkit.org>
+ *  Copyright (C) 2026 Igalia S.L.
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
@@ -109,6 +110,7 @@
 #include <wtf/Scope.h>
 #include <wtf/SetForScope.h>
 #include <wtf/SimpleStats.h>
+#include <wtf/SpinBackoff.h>
 #include <wtf/SystemTracing.h>
 #include <wtf/TZoneMallocInlines.h>
 #include <wtf/Threading.h>
@@ -1967,6 +1969,7 @@ NEVER_INLINE void Heap::resumeThePeriphery()
             visitorsToUpdate.append(&visitor);
         });
     
+    SpinBackoff backoff;
     for (unsigned countdown = 40; !visitorsToUpdate.isEmpty() && countdown--;) {
         for (unsigned index = 0; index < visitorsToUpdate.size(); ++index) {
             SlotVisitor& visitor = *visitorsToUpdate[index];
@@ -1983,7 +1986,7 @@ NEVER_INLINE void Heap::resumeThePeriphery()
                 visitorsToUpdate.takeLast();
             }
         }
-        Thread::yield();
+        backoff.spinOnce();
     }
     
     for (SlotVisitor* visitor : visitorsToUpdate)
